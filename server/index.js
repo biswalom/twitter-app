31f1d6edcced3fcde75891099894654ad4b0a9e2
om biswal <biswalom4@gmail.com>
2021-01-14 12:21:16 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -105,6 +105,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000 , () => {
   console.log('Listening on http://localhost:5000');
 });
